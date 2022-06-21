436ae64ea33cf66ad77c74f643e6e9f83bc87759
Alex1H <sasha.holovenko@gmail.com>
2022-06-21 17:10:46 +0300

    react hw 1

--- src/components/character.js
+++ src/components/character.js
@@ -1,22 +1,12 @@
-// function Character(props){
-//     let {id, name, story, picture} = props;
-//     return(
-//         <div>
-//             <h2>{name}</h2>
-//             <p>{story}</p>
-//             <img src= {picture} alt= ""/>
-//         </div>
-//     )
-// }
-export default Character;
-
 function Character(props){
-    let {id, name, status, species, gender, image} = props;
+    let {id, name, story, picture} = props;
     return(
         <div>
-            <h3>{id}</h3>
-
-
+            <h2>{name}</h2>
+            <p>{story}</p>
+            <img src= {picture} alt= ""/>
         </div>
     )
 }
+export default Character;
+
