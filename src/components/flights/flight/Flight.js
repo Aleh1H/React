d96cc02b3985a006de771dc5720c09b1646a7563
Alex1H <sasha.holovenko@gmail.com>
2022-06-24 00:26:50 +0300

    react hw 2 flights

--- src/components/flights/flight/Flight.js
+++ src/components/flights/flight/Flight.js
@@ -2,9 +2,10 @@ export default function Flight({item}) {
 
     return (
         <div>
-            <p> <b>Mission name</b> - {item.mission_name} <b>Launch year</b> - {item.launch_year} <br/> <b>mission patch</b> - {item.links.mission_patch_small}</p>
+            <p> <b>Mission name</b> - {item.mission_name}
+               <br/> <b>Launch year</b> - {item.launch_year}</p>
+                < img src = {item.links.mission_patch_small} alt={mission_name}/>
 
         </div>);
 }
 
-//
