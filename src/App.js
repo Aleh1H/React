bc6fcb244d8dc1528f160a1dd748c6b50f78a23e
Alex1H <sasha.holovenko@gmail.com>
2022-06-30 16:41:38 +0300

    react hw 2 flights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import {useForm} from "react-hook-form";
+import {saveUser} from "./services/user.services";
 
-const App = () => {
+export default function App() {
+
+    let {register, handleSubmit} = useForm({
+        defaultValues: {id: 'user id', name: 'name', username: 'username', email: 'email'}
+    });
+    let submit = (data) => {
+        console.log(data)
+        saveUser(data).then(({data}) => console.log(data));
+    }
+
+
     return (
         <div>
-          App
-        </div>
-    );
-};
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" {...register("id")}/>
+                <input type="text" {...register("name")}/>
+                <input type="text" {...register("username")}/>
+                <input type="text" {...register("email")}/>
+                <hr/>
+                <h4><i>address</i></h4>
+                <input type="text" {...register("address.street")}/>
+                <input type="text" {...register("address.city")}/>
+                <input type="text" {...register("address.zipcode")}/>
+                <input type="text" {...register("address.geo.lat")}/>
+                <input type="text" {...register("address.geo.lng")}/>
+                <hr/>
+                <input type="text" {...register("phone")}/>
+                <input type="text" {...register("website")}/>
+                <input type="text" {...register("company.name")}/>
+                <input type="text" {...register("company.catchPhrase")}/>
+                <input type="text" {...register("company.bs")}/>
 
-export default App;
+                <button>save</button>
+            </form>
+
+
+        </div>);
+}
