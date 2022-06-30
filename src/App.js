dce1b594afa8b6948a6b91dd844886a023e7f2c6
Alex1H <sasha.holovenko@gmail.com>
2022-06-30 19:01:24 +0300

    react hw 2 flights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import {useForm} from "react-hook-form";
+import {saveComment} from "./comments.services/comments.services";
 
-const App = () => {
+export default function App() {
+
+let {register, handleSubmit} = useForm();
+
+let add = (data) => {
+    console.log(data)
+    saveComment (data).then(({data}) => console.log(data))
+}
+
     return (
         <div>
-          App
-        </div>
-    );
-};
-
-export default App;
+            <form onSubmit={handleSubmit(add)}>
+                <input type="text" {...register('postId')}/>
+                <input type="number" {...register('id')}/>
+                <input type="text" {...register('name')}/>
+                <input type="email" {...register('email', {})}/>
+                <input type="text" {...register('body')}/>
+                <br/>
+                <button>submit</button>
+            </form>
+        </div>);
+}
